4c2724837884d467058b3193fc2798d79383d638
qk <qinkuan1987@163.com>
2020-02-15 20:27:58 +0800

    定义Label类

--- apps/labels/models.py
+++ apps/labels/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+
+class Label(models.Model):
+    name = models.CharField(max_length=20, null=False, blank=False, verbose_name="标签名称")
+    intro = models.TextField(null=False, blank=False, verbose_name="标签介绍")
+    parent_label = models.ForeignKey(to="self", null=True, verbose_name="父级标签")
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        db_table = "db_labels"
+        verbose_name = "标签"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
