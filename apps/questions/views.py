c76a119ae4e62eedb2d122345e8d1936bcb279ef
qk <qinkuan1987@163.com>
2020-02-18 22:03:05 +0800

    实现修改本人的回答

--- apps/questions/views.py
+++ apps/questions/views.py
@@ -1,5 +1,6 @@
 from apps.utils.api import CustomAPIView
 from .serializers import QuestionCreateSerializer, NewQuestionSerializer, AnswerCreateSerializer
+from .models import Answer
 
 
 class QuestionView(CustomAPIView):
@@ -49,6 +50,26 @@ class AnswerView(CustomAPIView):
             return self.invalid_serializer(s)
         try:
             instance = s.create(s.validated_data)
+        except Exception as e:
+            return self.error(e.args, 401)
+
+        data = dict(AnswerCreateSerializer(instance=instance).data)
+        data.pop("user_id")
+        data["who_answers"] = who_answers
+        return self.success(data)
+
+    def put(self, request, question_id):
+        """修改回答，只能修改本人的回答"""
+
+        user = request.user  # TODO 检查用户权限
+        user_id = "cd2ed05828ebb648a225c35a9501b007"  # TODO 虚假的ID
+        who_answers = "大师"  # TODO 虚假的名称
+
+        content = request.data.get("content", None)
+        try:
+            instance = Answer.objects.get(question=question_id, user_id=user_id)
+            instance.content = content
+            instance.save()
         except Exception as e:
             return self.error(e.args, 401)
 
