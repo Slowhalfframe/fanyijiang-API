4f856ea5b6a825ca60b4018709ac179e7498dc4c
qk <qinkuan1987@163.com>
2020-02-19 20:43:16 +0800

    撤销未回答的邀请

--- apps/questions/views.py
+++ apps/questions/views.py
@@ -1,7 +1,7 @@
 from apps.utils.api import CustomAPIView
 from .serializers import QuestionCreateSerializer, NewQuestionSerializer, AnswerCreateSerializer, \
     QuestionFollowSerializer, FollowedQuestionSerializer, InviteCreateSerializer
-from .models import Answer, QuestionFollow
+from .models import Answer, QuestionFollow, QuestionInvite
 
 
 class QuestionView(CustomAPIView):
@@ -161,3 +161,21 @@ class InvitationView(CustomAPIView):
             return self.error(e.args, 401)
         s = InviteCreateSerializer(instance=instance)
         return self.success(s.data)
+
+    def delete(self, request):
+        """撤销邀请，只能撤销本人发出的未回答的邀请"""
+
+        user = request.user  # TODO 检查用户权限
+        user_id = "cd2ed05828ebb648a225c35a9501b007"  # TODO 虚假的ID
+
+        data = {
+            "question": request.data.get("question", None),
+            "inviting": user_id,
+            "invited": request.data.get("invited", None),  # TODO 被邀请者，暂时采用ID
+            "status": 0  # 未回答
+        }
+        try:
+            QuestionInvite.objects.get(**data).delete()
+        except Exception as e:
+            return self.error(e.args, 401)
+        return self.success()
