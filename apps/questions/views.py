8075fc66acff0849ec375c8d4d9f77ae5e9314a7
qk <qinkuan1987@163.com>
2020-02-20 13:30:16 +0800

    修改评论

--- apps/questions/views.py
+++ apps/questions/views.py
@@ -269,3 +269,20 @@ class CommentView(CustomAPIView):
         except Exception as e:
             return self.error(e.args, 401)
         return self.success()
+
+    def patch(self, request):
+        """修改本人发表的问答评论"""
+
+        user = request.user  # TODO 检查用户权限
+        user_id = "cd2ed05828ebb648a225c35a9501b007"  # TODO 虚假的ID
+
+        pk = request.data.get("pk", None)
+        content = request.data.get("content", None)
+        try:
+            comment = QAComment.objects.get(pk=pk, user_id=user_id)
+            comment.content = content
+            comment.save()
+        except Exception as e:
+            return self.error(e.args, 401)
+        s = QACommentCreateSerializer(instance=comment)
+        return self.success(s.data)
