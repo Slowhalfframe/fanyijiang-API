394711d3df9099db9a5d31aa67549c4f8134f8e9
qk <qinkuan1987@163.com>
2020-02-23 09:41:02 +0800

    发表文章

--- apps/articles/serializers.py
+++ apps/articles/serializers.py
@@ -1,1 +1,46 @@
+import os
+import html
+
 from rest_framework import serializers
+
+from .models import Article
+from apps.labels.models import Label
+
+
+class ArticleCreateSerializer(serializers.ModelSerializer):
+    """只用于反序列化验证和保存"""
+    labels = serializers.ListField(required=True)
+
+    class Meta:
+        model = Article
+        fields = ("user_id", "title", "content", "image", "status", "labels",)
+
+    def validate_labels(self, value):
+        value = Label.objects.filter(name__in=value)
+        if not value:
+            raise serializers.ValidationError("标签不存在")
+        return value
+
+    def validate_image(self, value):
+        # TODO 应该检查以确保文件是存在的，至少是合格的文件路径
+        path, ext = os.path.splitext(value)
+        if not ext:
+            raise serializers.ValidationError("文件名为空")
+        if ext not in (".gif", ".bmp", ".jpg", ".jpeg", ".png"):
+            raise serializers.ValidationError("无效的图片类型")
+        return value
+
+    def validate_content(self, value):
+        value = html.escape(value)
+        return value
+
+
+class NewArticleSerializer(serializers.ModelSerializer):
+    """只用于序列化输出"""
+    create_at = serializers.DateTimeField(format="%Y%m%d %H:%M:%S", read_only=True)
+    update_at = serializers.DateTimeField(format="%Y%m%d %H:%M:%S", read_only=True)
+    labels = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Article
+        fields = ("pk", "user_id", "title", "content", "image", "status", "create_at", "update_at", "labels",)
--- apps/articles/views.py
+++ apps/articles/views.py
@@ -1,1 +1,30 @@
 from apps.utils.api import CustomAPIView
+from .serializers import ArticleCreateSerializer, NewArticleSerializer
+
+
+class ArticleView(CustomAPIView):
+    def post(self, request):
+        """发表文章"""
+
+        user = request.user  # TODO 检查用户权限
+        user_id = "cd2ed05828ebb648a225c35a9501b007"  # TODO 虚假的ID
+
+        data = {
+            "user_id": user_id,
+            "title": request.data.get("title", None),
+            "content": request.data.get("content", None),  # 注意HTML转义
+            "image": request.data.get("image", None),
+            "status": request.data.get("status", "draft"),
+            "labels": request.data.getlist("labels", []),
+        }
+        s = ArticleCreateSerializer(data=data)
+        s.is_valid()
+        if s.errors:
+            return self.invalid_serializer(s)
+
+        try:
+            article = s.create(s.validated_data)
+        except Exception as e:
+            return self.error(e.args, 401)
+        s = NewArticleSerializer(instance=article)
+        return self.success(s.data)
