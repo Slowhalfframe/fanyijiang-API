928a5015f1086c7ef6514eeb6ab745cef7688214
qk <qinkuan1987@163.com>
2020-02-28 18:04:07 +0800

    发表想法

--- apps/ideas/serializers.py
+++ apps/ideas/serializers.py
@@ -1,1 +1,23 @@
 from rest_framework import serializers
+
+from .models import Idea
+
+
+class IdeaValidator(serializers.ModelSerializer):
+    class Meta:
+        model = Idea
+        fields = ("user_id", "content")
+
+
+class IdeaDetailSerializer(serializers.ModelSerializer):
+    create_at = serializers.DateTimeField(format="%Y%m%d %H:%M:%S")
+    agree_count = serializers.SerializerMethodField()
+    nickname = serializers.CharField()
+    avatar = serializers.CharField()
+
+    class Meta:
+        model = Idea
+        fields = ("user_id", "content", "create_at", "pk", "agree_count", "nickname", "avatar")
+
+    def get_agree_count(self, obj):
+        return obj.agree.count()
--- apps/ideas/views.py
+++ apps/ideas/views.py
@@ -1,1 +1,30 @@
 from apps.utils.api import CustomAPIView
+
+from .serializers import IdeaValidator, IdeaDetailSerializer
+
+
+class IdeaView(CustomAPIView):
+    def post(self, request):
+        """发表想法"""
+
+        user = request.user  # TODO 检查用户权限
+        user_id = "cd2ed05828ebb648a225c35a9501b007"  # TODO 虚假的ID
+        avatar = "images/001.png"  # TODO 虚假的头像
+        nickname = "新手"  # 虚假的昵称
+
+        data = {
+            "user_id": user_id,
+            "content": request.data.get("content", None)
+        }
+        s = IdeaValidator(data=data)
+        s.is_valid()
+        if s.errors:
+            return self.invalid_serializer(s)
+        try:
+            idea = s.create(s.validated_data)
+        except Exception as e:
+            return self.error(e.args, 401)
+        idea.avatar = avatar
+        idea.nickname = nickname
+        s = IdeaDetailSerializer(instance=idea)
+        return self.success(s.data)
