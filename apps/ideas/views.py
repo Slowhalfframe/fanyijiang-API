4d6d9332b16ff27af61a63d256e078b6ce189466
qk <qinkuan1987@163.com>
2020-02-29 09:41:38 +0800

    查看本人的想法

--- apps/ideas/views.py
+++ apps/ideas/views.py
@@ -30,6 +30,24 @@ class IdeaView(CustomAPIView):
         s = IdeaDetailSerializer(instance=idea)
         return self.success(s.data)
 
+    def get(self, request):
+        """查看本人的所有想法"""
+
+        user = request.user  # TODO 检查用户权限
+        user_id = "cd2ed05828ebb648a225c35a9501b007"  # TODO 虚假的ID
+        avatar = "images/001.png"  # TODO 虚假的头像
+        nickname = "新手"  # TODO 虚假的昵称
+
+        try:
+            ideas = Idea.objects.filter(user_id=user_id)
+        except Exception as e:
+            return self.error(e.args, 401)
+        for idea in ideas:
+            idea.avatar = avatar
+            idea.nickname = nickname
+        s = IdeaDetailSerializer(instance=ideas, many=True)
+        return self.success(s.data)
+
 
 class MonoIdeaView(CustomAPIView):
     def delete(self, request, idea_pk):
