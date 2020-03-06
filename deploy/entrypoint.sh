4a4b8033265d30a941acc48c6a62e8574314f38f
zjc <zjc>
2020-03-06 13:35:15 +0800

    修改部署文件

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 APP=/app
 cd $APP
+mkdir -p deploy/logs/
 touch deploy/logs/supervisord.log
+for a in `ls $APP"/"apps`
+  do
+    if [ -d $APP"/"apps"/"$a ]
+    then
+      cd $APP"/"apps"/"$a
+      mkdir migrations
+      cd migrations
+      touch __init__.py
+    fi
+  done
+if [ -f $APP"/"deploy"/"celerybeat.pid ]; then
+rm -r $APP"/"deploy"/"celerybeat.pid
+fi
+
+if [ -f $APP"/"deploy"/"celerybeat-schedule ]; then
+rm -r $APP"/"deploy"/"celerybeat-schedule
+fi
+
+
+cd $APP
+python manage.py makemigrations && python manage.py migrate
 exec supervisord -c /app/deploy/supervisord.conf
