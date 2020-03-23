db7934181e690289ee677a6c03aebd0387e0dc72
qk <qinkuan1987@163.com>
2020-03-23 13:41:02 +0800

    标签子应用的模型类

--- apps/labels_v2/models.py
+++ apps/labels_v2/models.py
@@ -1,1 +1,36 @@
 from django.db import models
+
+from apps.utils.models import BaseModel
+from apps.userpage.models import UserProfile
+
+
+class Label(BaseModel):
+    """标签，或者称作话题，名称不可重复"""
+
+    name = models.CharField(max_length=20, unique=True, null=False, blank=False, verbose_name="标签名称")
+    intro = models.TextField(null=False, blank=True, default="", verbose_name="标签介绍")
+    avatar = models.CharField(max_length=100, blank=True, null=False, default="", verbose_name="标签头像")
+    children = models.ManyToManyField(to="self", symmetrical=False, verbose_name="子标签", related_name="parents")
+
+    class Meta:
+        db_table = "label"
+        verbose_name = "标签"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class LabelFollow(BaseModel):
+    """用户与标签的多对多关注关系"""
+
+    user = models.ForeignKey(to=UserProfile, null=False, verbose_name="关注者ID")
+    label = models.ForeignKey(to=Label, null=False, verbose_name="关注的标签")
+
+    class Meta:
+        db_table = "label_follow"
+        verbose_name = "标签关注"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return "".join((self.user.nickname, "跟踪了", self.label.name))
