0432f454cf0f96a6f425eaec5b74135a34284d0a
zjc <1149327410@qq.com>
2020-03-24 10:18:35 +0800

    增加admin管理

--- apps/articles/admin.py
+++ apps/articles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Article)
+admin.site.register(ArticleComment)
+admin.site.register(ArticleVote)
--- apps/ideas/admin.py
+++ apps/ideas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Idea)
+admin.site.register(IdeaComment)
+admin.site.register(IdeaLike)
