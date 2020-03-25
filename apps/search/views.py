bc1907188b5a7476f9629a6d3bf109d7fb0925dc
qk <qinkuan1987@163.com>
2020-03-25 17:51:45 +0800

    搜索到的用户和回答的格式

--- apps/search/views.py
+++ apps/search/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ViewSetMixin
 
 from apps.questions.models import Question, Answer
-from apps.questions.serializers import NewQuestionSerializer, AnswerCreateSerializer
+from apps.questions.serializers import NewQuestionSerializer, SearchAnswersSerializer
 from apps.articles.models import Article
 from apps.articles.serializers import ArticleDetailSerializer
 from apps.ideas.models import Idea
@@ -25,16 +25,20 @@ class RootView(ViewSetMixin, HaystackGenericAPIView, CustomAPIView):
         result = []
         mapping = {
             Question.__name__: NewQuestionSerializer,
-            Answer.__name__: AnswerCreateSerializer,
+            Answer.__name__: SearchAnswersSerializer,
             Article.__name__: ArticleDetailSerializer,
             Idea.__name__: IdeaDetailSerializer
         }
         for i in queryset:
             instance = i.object
-            s = mapping[instance.__class__.__name__](instance=instance, context={"me": me})
+            if isinstance(instance,Answer):
+                s = mapping[instance.__class__.__name__](instance={"answer":instance}, context={"me": me})
+            else:
+                s = mapping[instance.__class__.__name__](instance=instance, context={"me": me})
             data = dict(s.data)
             data["kind"] = instance.__class__.__name__.lower()
             result.append(data)
+
 
         data = {"result": result, "total": len(queryset)}
         return self.success(data)
@@ -49,4 +53,10 @@ class FindUserView(ViewSetMixin, HaystackGenericAPIView, CustomAPIView):
         queryset = self.filter_queryset(self.get_queryset())
         queryset = [i.object for i in queryset]
         data = self.paginate_data(request, queryset, self.get_serializer_class())
+        if not me:
+            for r in data["results"]:
+                r["is_idol"] = False
+        else:
+            for r in data["results"]:
+                r["is_idol"] = UserProfile.objects.filter(as_idol__fans__uid=me.uid, slug=r['slug']).exists()
         return self.success(data)
