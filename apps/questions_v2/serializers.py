9765c05362fb68de0e39f1cfcb2b75519130cb14
qk <qinkuan1987@163.com>
2020-03-28 00:23:25 +0800

    问题的序列化器

--- apps/questions_v2/serializers.py
+++ apps/questions_v2/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from apps import xss_safe
 from apps.labels_v2.serializers import BasicLabelSerializer
-from apps.questions_v2.models import Question
+from apps.questions_v2.models import Question, Answer
 from apps.userpage.models import UserProfile
 
 
@@ -95,4 +95,70 @@ class MeQuestionSerializer(StatQuestionSerializer):
         return False  # TODO 返回真实数据
 
     def get_is_answered(self, obj):
+        return False  # TODO 返回真实数据
+
+
+class AnswerChecker(serializers.ModelSerializer):
+    """用于新建或修改回答时检查数据"""
+
+    class Meta:
+        model = Answer
+        fields = ("content", "is_draft",)
+
+    def validate_content(self, value):
+        if not value or value.capitalize() == str(None):
+            raise serializers.ValidationError("回答没有内容")
+        return value
+
+    def validate_is_draft(self, value):
+        if value not in (True, False):
+            raise serializers.ValidationError("没有指明回答是否是草稿")
+        return value
+
+
+class BasicAnswerSerializer(serializers.ModelSerializer):
+    """用于回答的序列化，返回最基础的信息"""
+
+    type = serializers.CharField(source="kind")
+    create_at = serializers.DateTimeField(format="%Y%m%d %H:%M:%S")
+    update_at = serializers.DateTimeField(format="%Y%m%d %H:%M:%S")
+    question = BasicQuestionSerializer()
+    author = SimpleUserSerializer()
+
+    class Meta:
+        model = Answer
+        fields = ("id", "type", "content", "is_draft", "question", "author", "create_at", "update_at",)
+
+
+class StatAnswerSerializer(BasicAnswerSerializer):
+    """用于回答的序列化，增加了与登录用户无关的统计信息"""
+
+    comment_count = serializers.SerializerMethodField()  # TODO 评论数
+    vote_count = serializers.SerializerMethodField()  # TODO 赞成票数
+
+    class Meta:
+        model = Answer
+        fields = BasicAnswerSerializer.Meta.fields + ("comment_count", "vote_count",)
+
+    def get_comment_count(self, obj):
+        return 100  # TODO 返回真实数据
+
+    def get_vote_count(self, obj):
+        return 100  # TODO 返回真实数据
+
+
+class MeAnswerSerializer(StatAnswerSerializer):
+    """用于回答的序列化，增加了与登录用户有关的信息，需要传入当前登录用户"""
+
+    is_voted = serializers.SerializerMethodField()  # TODO 是否已经投票，或票值
+    is_commented = serializers.SerializerMethodField()  # TODO 是否已经评论
+
+    class Meta:
+        model = Question
+        fields = StatQuestionSerializer.Meta.fields + ("is_voted", "is_commented",)
+
+    def get_is_voted(self, obj):
+        return None  # TODO 返回真实数据
+
+    def get_is_commented(self, obj):
         return False  # TODO 返回真实数据
