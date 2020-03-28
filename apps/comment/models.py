69417a531604743bd70f4bbbe62cf0b06cc21a56
qk <qinkuan1987@163.com>
2020-03-28 19:35:43 +0800

    评论的模型和序列化器、投票的模型

--- apps/comment/models.py
+++ apps/comment/models.py
@@ -1,3 +1,51 @@
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
-# Create your models here.
+from apps.userpage.models import UserProfile
+from apps.utils.models import BaseModel
+
+
+class Comment(BaseModel):
+    """评论，适用于问题、问题的回答、文章、想法等，以及评论的评论"""
+
+    content = models.TextField(null=False, blank=False, verbose_name="评论内容")
+    author = models.ForeignKey(to=UserProfile, null=False, blank=False, verbose_name="评论者")
+    content_type = models.ForeignKey(to=ContentType, null=False, verbose_name="被评论对象的类型")
+    object_id = models.PositiveIntegerField(null=False, verbose_name="被评论对象的主键")
+    content_object = GenericForeignKey("content_type", "object_id")
+    comments = GenericRelation(to="self")  # 用于获取评论的子评论
+    votes = GenericRelation(to="Vote")  # 用于获取评论的投票
+
+    class Meta:
+        db_table = "comment"
+        verbose_name = "评论"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.content[:20]
+
+    @property
+    def kind(self):
+        return "comment"
+
+    @property
+    def respondent(self):
+        """被回复者"""
+
+        return self.content_object.author
+
+
+class Vote(BaseModel):
+    """投票，泛指各种取True或False但不存在时应看作None的事物，操作时注意不能重复投票"""
+
+    value = models.BooleanField(null=False, blank=False, verbose_name="赞成还是反对")
+    author = models.ForeignKey(to=UserProfile, null=False, blank=False, verbose_name="投票者")
+    content_type = models.ForeignKey(to=ContentType, null=False, verbose_name="被投票对象的类型")
+    object_id = models.PositiveIntegerField(null=False, verbose_name="被投票对象的主键")
+    content_object = GenericForeignKey("content_type", "object_id")
+
+    class Meta:
+        db_table = "vote"
+        verbose_name = "投票"
+        verbose_name_plural = verbose_name
--- apps/comment/serializers.py
+++ apps/comment/serializers.py
@@ -1,1 +1,66 @@
 from rest_framework import serializers
+
+from apps.questions_v2.serializers import SimpleUserSerializer
+from .models import Comment
+
+
+class CommentChecker(serializers.ModelSerializer):
+    """用于新建或修改评论时检查数据"""
+
+    class Meta:
+        model = Comment
+        fields = ("content",)
+
+    def validate_content(self, value):
+        if not value or value.capitalize() == str(None):
+            return None
+        return value
+
+
+class BasicCommentSerializer(serializers.ModelSerializer):
+    """用于评论的序列化，返回最基础的信息"""
+
+    type = serializers.CharField(source="kind")
+    author = SimpleUserSerializer()
+    respondent = SimpleUserSerializer()
+    create_at = serializers.DateTimeField(format="%Y%m%d %H:%M:%S")
+    update_at = serializers.DateTimeField(format="%Y%m%d %H:%M:%S")
+
+    class Meta:
+        model = Comment
+        fields = ("id", "type", "content", "author", "respondent", "create_at", "update_at",)
+
+
+class StatCommentSerializer(BasicCommentSerializer):
+    """用于评论的序列化，增加了与登录用户无关的统计信息和子评论的基本信息"""
+
+    children = BasicCommentSerializer(many=True)
+    comment_count = serializers.SerializerMethodField()  # TODO 子评论数
+    vote_count = serializers.SerializerMethodField()  # TODO 赞成票数
+
+    class Meta:
+        model = Comment
+        fields = BasicCommentSerializer.Meta.fields + ("children", "vote_count", "comment_count",)
+
+    def get_comment_count(self, obj):
+        return 100  # TODO 返回真实数据
+
+    def get_vote_count(self, obj):
+        return 100  # TODO 返回真实数据
+
+
+class MeCommentSerializer(StatCommentSerializer):
+    """用于评论的序列化，增加了与登录用户有关的信息，需要传入当前登录用户"""
+
+    is_voted = serializers.SerializerMethodField()  # TODO 是否已经投票，或票值
+    is_commented = serializers.SerializerMethodField()  # TODO 是否已经评论
+
+    class Meta:
+        model = Comment
+        fields = StatCommentSerializer.Meta.fields + ("is_voted", "is_commented",)
+
+    def get_is_voted(self, obj):
+        return None  # TODO 返回真实数据
+
+    def get_is_commented(self, obj):
+        return False  # TODO 返回真实数据
