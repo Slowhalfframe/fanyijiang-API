a2ffcfe49002ac44f4521ee6b7e496e7c26f8441
qk <qinkuan1987@163.com>
2020-03-30 14:45:46 +0800

    文章模型和序列化器

--- apps/articles_v2/models.py
+++ apps/articles_v2/models.py
@@ -1,1 +1,38 @@
+from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
+
+from apps.comments.models import Comment
+from apps.labels_v2.models import Label
+from apps.userpage.models import UserProfile
+from apps.utils.models import BaseModel
+from apps.votes.models import Vote
+
+
+class Article(BaseModel):
+    """文章，同一作者可以发表多篇同一标题的文章，尽管这几乎不会发生"""
+
+    STATUS = (("draft", "draft"), ("published", "published"))
+
+    title = models.CharField(max_length=100, null=False, blank=False, verbose_name="文章标题")
+    content = models.TextField(null=False, blank=False, verbose_name="文章主体")
+    thumbnail = models.CharField(max_length=100, null=True, verbose_name="缩略图路径")
+    status = models.CharField(max_length=10, choices=STATUS, null=False, blank=False, verbose_name="状态")
+    author = models.ForeignKey(to=UserProfile, null=False, blank=False, verbose_name="作者")
+    labels = models.ManyToManyField(to=Label, verbose_name="文章的标签")
+    comments = GenericRelation(to=Comment)
+    votes = GenericRelation(to=Vote)
+
+    # mark = GenericRelation(to=FavoriteCollection, verbose_name="收藏")
+    # read_nums = GenericRelation(to=ReadNums)
+
+    class Meta:
+        db_table = "article"
+        verbose_name = "文章"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def kind(self):
+        return "answer"
--- apps/articles_v2/serializers.py
+++ apps/articles_v2/serializers.py
@@ -1,1 +1,115 @@
 from rest_framework import serializers
+
+from apps import xss_safe, legal_image_path
+from apps.comments.serializers import BasicUserSerializer
+from apps.labels_v2.serializers import BasicLabelSerializer
+from .models import Article
+
+
+class ArticleChecker(serializers.ModelSerializer):
+    """用于新建或修改文章时检查数据"""
+
+    class Meta:
+        model = Article
+        fields = ("title", "content", "thumbnail", "status",)
+        extra_kwargs = {
+            "title": {
+                "required": True,
+            },
+            "content": {
+                "required": True,
+            },
+            "thumbnail": {
+                "required": False,
+            },
+            "status": {
+                "required": True,
+            },
+        }
+
+    def validate_title(self, value):
+        if not value or value.capitalize() == str(None):
+            raise serializers.ValidationError("文章标题不能为空")
+        if not xss_safe(value):
+            raise serializers.ValidationError("文章标题含有特殊字符")
+        return value
+
+    def validate_content(self, value):
+        if not value or value.capitalize() == str(None):
+            raise serializers.ValidationError("文章主体不能为空")
+        return value
+
+    def validate_thumbnail(self, value):
+        if not value or value.capitalize() == str(None):
+            return None
+        if not legal_image_path(value):
+            raise serializers.ValidationError("图片路径或类型无效")
+        return value
+
+    def validate_status(self, value):
+        if value not in ("draft", "published"):
+            raise serializers.ValidationError("文章状态不合要求")
+        return value
+
+
+class BasicArticleSerializer(serializers.ModelSerializer):
+    """用于文章的序列化，返回最基础的信息"""
+
+    type = serializers.CharField(source="kind")
+    author = BasicUserSerializer()
+    labels = BasicLabelSerializer(many=True)
+    create_at = serializers.DateTimeField(format="%Y%m%d %H:%M:%S")
+    update_at = serializers.DateTimeField(format="%Y%m%d %H:%M:%S")
+
+    class Meta:
+        model = Article
+        fields = (
+            "id", "type", "title", "content", "thumbnail", "status", "author", "labels", "create_at", "update_at",
+        )
+
+
+class StatArticleSerializer(BasicArticleSerializer):
+    """用于文章的序列化，增加了与登录用户无关的统计信息"""
+
+    comment_count = serializers.SerializerMethodField()
+    vote_count = serializers.SerializerMethodField()
+
+    # TODO 阅读数，关注者个数
+
+    class Meta:
+        model = Article
+        fields = BasicArticleSerializer.Meta.fields + ("comment_count", "vote_count",)
+
+    def get_comment_count(self, obj):
+        return obj.comments.filter(is_deleted=False).count()
+
+    def get_vote_count(self, obj):
+        return obj.votes.filter(value=True).count()
+
+
+class MeArticleSerializer(StatArticleSerializer):
+    """用于文章的序列化，增加了与登录用户有关的信息，需要传入当前登录用户"""
+
+    is_voted = serializers.SerializerMethodField()  # 未投票，或票值
+    is_commented = serializers.SerializerMethodField()
+
+    # TODO 是否增加is_me，表示登录者即作者？需要重写author属性
+    # TODO 是否关注
+    class Meta:
+        model = Article
+        fields = StatArticleSerializer.Meta.fields + ("is_voted", "is_commented",)
+
+    def get_is_voted(self, obj):
+        me = self.context.get("me")
+        if me is None:
+            return None
+        my_vote = obj.votes.filter(author=me).first()
+        if my_vote is None:
+            return None
+        return my_vote.value
+
+    def get_is_commented(self, obj):
+        me = self.context.get("me")
+        if me is None:
+            return False
+        return obj.comments.filter(author=me, is_deleted=False).exists()
