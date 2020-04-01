73645504173835a2e2f7e011329dbc9cff7fe410
zjc <1149327410@qq.com>
2020-04-01 11:58:29 +0800

    update

--- apps/userpage/urls.py
+++ apps/userpage/urls.py
@@ -47,6 +47,8 @@ urlpatterns = [
     # 接受统一用户中心发送过来的更新请求
     url(r"^uc_update$", views.UcUpdateAPIView.as_view(), name="uc_update"),
 
+    url(r"^page_image$", views.UserPageImagePUTAPIView.as_view(), name="page_image"),
+
     url(r'hover_userinfo/(?P<user_slug>[-\w]+)$', views.HoverUserInfoAPIView.as_view(), name='hover_userinfo'),
     url(r'hover_labelinfo/(?P<pk>\d+)$', views.HoverLabelInfoAPIView.as_view(), name='hover_userinfo'),
 
